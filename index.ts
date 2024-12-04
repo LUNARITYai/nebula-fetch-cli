4ac3a480f08b91be21ebaa07fbfb2b6202b39fb3
Krzysztof Oszczapiński <krzysztof.oszczapinski@pagepro.pl>
2024-12-04 11:00:39 +0100

    Added youtube download command

--- index.ts
+++ index.ts
@@ -1,59 +1,29 @@
-import fs from "fs";
-import path from "path";
+#!/usr/bin/env node
 
+import { Command } from "commander";
 import chalk from "chalk";
-import { YtdlCore, toPipeableStream } from "@ybd-project/ytdl-core";
+import { downloadYoutubeAudio } from "./commands/youtube";
 
-const DIRECTORIES = {
-  audio: "./audio",
-};
+const program = new Command();
 
-async function downloadYoutubeAudio(url: string): Promise<void> {
-  try {
-    if (!url) {
-      throw new Error("Please provide a YouTube URL");
+program
+  .name("nebula-fetch")
+  .description("CLI tool for downloading media from different platforms")
+  .version("1.0.0");
+
+program
+  .command("youtube")
+  .description("Download a video from YouTube")
+  .argument("<url>", "URL of the video")
+  .option("-o, --output <path>", "Output path for the video")
+  .action(async (url, options) => {
+    try {
+      console.log(chalk.blue(`Downloading video from: ${url}`));
+      await downloadYoutubeAudio(url, options.output);
+    } catch (error) {
+      console.error(chalk.red("Error:"), error);
+      process.exit(1);
     }
+  });
 
-    console.log(chalk.cyan("🔍 Fetching video information..."));
-
-    const ytdl = new YtdlCore({});
-
-    let videoTitle = "";
-
-    ytdl.getBasicInfo(url).then((info) => {
-      videoTitle = info.videoDetails.title.replace(/[^\w\s]/gi, "_");
-    });
-
-    const stream = await ytdl.download(url, {
-      filter: "audioonly",
-    });
-
-    const outputFilePath = path.join(DIRECTORIES.audio, `${videoTitle}.mp3`);
-
-    toPipeableStream(stream).pipe(fs.createWriteStream(outputFilePath));
-
-    console.log(chalk.green(`✅ Download finished: ${outputFilePath}`));
-  } catch (error) {
-    if (error instanceof Error) {
-      console.error(chalk.red("Error:", error.message));
-    }
-    process.exit(1);
-  }
-}
-
-// Get command line arguments
-const args = process.argv.slice(2);
-const urlArg = args.find((arg) => arg.startsWith("--url="));
-
-if (!urlArg) {
-  console.error(
-    chalk.red("Please provide a YouTube URL using --url=<youtube_url>")
-  );
-  process.exit(1);
-}
-
-const url = urlArg.split("--url=")[1];
-downloadYoutubeAudio(url).catch((error) => {
-  console.error(chalk.red("Fatal error:", error));
-  process.exit(1);
-});
+program.parse();
